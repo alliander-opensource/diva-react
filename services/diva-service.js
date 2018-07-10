acf8ab25592b7a30081261f122dfd2aee44b6c27
Koen van Ingen <koenvaningen@gmail.com>
2018-07-10 11:17:37 +0200

    chore: simplified loading of BASE_URL

--- services/diva-service.js
+++ services/diva-service.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
+const baseUrl = `${window.env.baseUrl}/api`;
+
 function startIrmaSession(irmaSessionType, options) {
   // TODO: send options and parse in backend based on type?
   // only properties that are passed to the service will actually be included in the request
   return axios
-    .post(`${window.env.baseUrl}/api/start-irma-session`, {
+    .post(`${baseUrl}/start-irma-session`, {
       type: irmaSessionType,
       content: options.attributesRequired,
       message: options.message,
@@ -21,7 +23,7 @@ function startIrmaSession(irmaSessionType, options) {
 
 function poll(irmaSessionType, irmaSessionId) {
   return axios
-    .get(`${window.env.baseUrl}/api/irma-session-status?irmaSessionType=${irmaSessionType}&irmaSessionId=${irmaSessionId}`, {
+    .get(`${baseUrl}/irma-session-status?irmaSessionType=${irmaSessionType}&irmaSessionId=${irmaSessionId}`, {
       withCredentials: true,
     })
     .then(response => response.data);
